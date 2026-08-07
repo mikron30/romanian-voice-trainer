e815aaf598de382fb93038adef5cc0780d8e158e
mikron30 <53993027+mikron30@users.noreply.github.com>
2026-08-07 21:37:14 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'romanian-voice-trainer-v11-topic-stats-weak-words';
+const CACHE_NAME = 'romanian-voice-trainer-v12-seven-word-batches';
 const BASE_URL = new URL('./', self.location.href).href;
 const APP_SHELL = [
   BASE_URL,
